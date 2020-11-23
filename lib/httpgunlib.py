6cd44170ddd3143d28a25b346a73843723dbe5b7
syyntax <syyntax@protonmail.com>
2020-11-23 14:18:59 -0600

    Modified httgunlib classes.

--- lib/httpgunlib.py
+++ lib/httpgunlib.py
@@ -1,34 +1,49 @@
 class Request:
-    def __init__(self, request_id, name, user_agent):
-        self.id = request_id
-        self.name = name
+    def __init__(self, obj_id, name=None, user_agent=None):
+        self.id = int(obj_id)
+        self.name = str(name)
         self.user_agent = user_agent
 
 
 class UserAgent:
-    def __init__(self, user_agent, op_sys, enabled, hardware, popularity):
-        self.user_agent = user_agent
+    def __init__(self, obj_id, name=None, op_sys=None, enabled=True, hardware=None, popularity=None):
+        self.id = int(obj_id)
+        self.name = str(name)
         self.OS = op_sys
-        self.enabled = enabled
+        self.enabled = bool(enabled)
         self.hardware = hardware
-        self.popularity = popularity
+        self.popularity = str(popularity)
 
-    def set_user_agent(self, user_agent):
-        self.user_agent = user_agent
-        return user_agent
+    def set_id(self, obj_id):
+        old = self.id
+        self.id = int(obj_id)
+        return f'ID: {old} --> {obj_id}'
+
+    def set_name(self, name):
+        old = self.name
+        self.name = str(name)
+        return f'User Agent: {old} --> {name}'
 
     def set_opsys(self, op_sys):
+        old = self.OS
         self.OS = op_sys
-        return op_sys
+        return f'OS: {old} --> {op_sys}'
 
     def set_enabled(self, enabled):
-        self.enabled = enabled
-        return enabled
+        if type(enabled) == bool:
+            old = self.enabled
+            self.enabled = enabled
+            return f'Enabled: {old} --> {enabled}'
+
+        else:
+            raise Exception(f'Parameter {enabled} is not bool type.')
 
     def set_hardware(self, hardware):
+        old = self.hardware
         self.hardware = hardware
-        return hardware
+        return f'Hardware: {old} --> {hardware}'
 
     def set_popularity(self, popularity):
+        old = self.popularity
         self.popularity = popularity
-        return popularity
+        return f'Popularity: {old} --> {popularity}'
